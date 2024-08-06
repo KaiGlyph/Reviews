1a82fb3de6ceeb4f0587562b494f8524a712cb6a
Jordi Gallardo <jordigallardo0621@gmail.com>
2024-08-06 17:28:25 +0200

    Añadido Acerca de

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+//Desplacamiento Suave de Navegacion
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('nav a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
